242a6a3722d85ebd7069005633923050115ded3f
AADIP2005 <Pandit.aadikanpurup@gmail.com>
2024-06-15 12:46:35 +0530

    assignment

---
+++ 1.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+sum=num1+num2
+print("the sum of given numbers :",sum)
+
+'''
+#SOLUTION
+PS C:\Users\aditi pandit\Desktop\PALERMO> & C:/msys64/ucrt64/bin/python.exe "c:/Users/aditi pandit/Desktop/PALERMO/1.py"
+Enter your first number:500
+Enter your second number:345
+the sum of given numbers : 845
+'''
---
+++ 2.py
@@ -0,0 +1,15 @@
+num=int(input("Enter the number:"))
+if num%2==0:
+    print("The given number is positive:",num)
+else:
+    print("The given number is odd:",num)
+
+#SOLUTION
+'''
+Enter the number:24
+The given number is positive: 24
+PS C:\Users\aditi pandit\Desktop\PALERMO> & C:/msys64/ucrt64/bin/python.exe "c:/Users/aditi pandit/Desktop/PALERMO/2.py"
+Enter the number:55
+The given number is odd: 55
+'''
+    
