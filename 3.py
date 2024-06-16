903c1c89831a5edaefc2d40f6b9b474289e189ec
AADIP2005 <Pandit.aadikanpurup@gmail.com>
2024-06-16 17:19:02 +0530

    assignment

--- 3.py
+++ 3.py
@@ -1,6 +1,15 @@
-num=int(input("enter your number:"))
-factorial=1
-if num>=1:
-    for i in range(1,num+1):
-        factorial=factorial*i
-        print(factorial)
+def factorial(n):
+    if (n==1 or n==0):
+        return 1
+    else:
+        a=n*factorial(n-1)
+        return a
+    
+num=int(input("enter the number:"))
+print("factorial of",num,"is",factorial(num))
+
+'''
+PS C:\Users\aditi pandit\Desktop\PALERMO> & C:/msys64/ucrt64/bin/python.exe "c:/Users/aditi pandit/Desktop/PALERMO/3.py"
+enter the number:3
+factorial of 3 is 6
+'''
